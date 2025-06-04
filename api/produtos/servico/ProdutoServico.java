e3c7dcc445c6313a294be9bf715e3e4c2d04736f
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-06-04 16:29:03 -0300

    Create ProdutoServico.java

---
+++ api/produtos/servico/ProdutoServico.java
@@ -0,0 +1,17 @@
+package br.com.api.produtos.servico;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.repositorio.ProdutoRepositorio;
+
+@Service
+public class ProdutoServico {
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
+}
