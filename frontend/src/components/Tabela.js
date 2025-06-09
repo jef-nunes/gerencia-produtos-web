afe1cc927dd4b208fbae9062583bfa85e13089f1
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-06-09 16:21:38 -0300

    Add files via upload

---
+++ frontend/src/components/Tabela.js
@@ -0,0 +1,16 @@
+function Tabela(){
+    return (
+        <table className="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Marca</th>
+                    <th>Selecionar</th>
+                </tr>
+            </thead>
+        </table>
+    )
+}
+
+export default Tabela;
