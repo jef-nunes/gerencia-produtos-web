e93766ee4a60d1ac2f3c8c83a142c4152d2169ba
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-06-09 16:22:07 -0300

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './css/App.css';
+import Formulario from './components/Formulario';
+import Tabela from './components/Tabela';
+
+
+function App() {
+  return (
+    <div className="App">
+    <Formulario/>
+    <Tabela/>
+    </div>
+  );
+}
+
+export default App;
