ccb343331f5748018c0dcd77397f63984f76ecf8
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-06-09 15:11:30 -0300

    Update ProdutoServico.java

--- api/produtos/servico/ProdutoServico.java
+++ api/produtos/servico/ProdutoServico.java
@@ -1,9 +1,12 @@
 package br.com.api.produtos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -11,7 +14,43 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio pr;
 
+    @Autowired
+    private RespostaModelo rm;
+
     public Iterable<ProdutoModelo> listar(){
         return pr.findAll();
+    }
+
+    public ResponseEntity<?> cadastrar(ProdutoModelo pm){
+        if(pm.getNome().equals("")){
+            rm.setMensagem("O nome do produto é obrigatório.");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }
+        else if(pm.getMarca().equals("")){
+            rm.setMensagem("A nome da marca é obrigatória.");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }
+        else{
+            return new ResponseEntity<ProdutoModelo>(pr.save(pm),HttpStatus.CREATED);
+        }
+    }
+
+    public ResponseEntity<?> cadastrarAlterar(ProdutoModelo pm, String acao){
+        if(pm.getNome().equals("")){
+            rm.setMensagem("O nome do produto é obrigatório.");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }
+        else if(pm.getMarca().equals("")){
+            rm.setMensagem("A nome da marca é obrigatória.");
+            return new ResponseEntity<RespostaModelo>(rm,HttpStatus.BAD_REQUEST);
+        }
+        else{
+            if(acao.equals("cadastrar")){
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+            }
+            else{
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm),HttpStatus.OK);
+            }
+        }
     }
 }
