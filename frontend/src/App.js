b310d87fab7fc2a15365ec44a7ab0fcd410e6d36
Jeferson <159483664+jef-nunes@users.noreply.github.com>
2025-06-09 16:34:21 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
 import './css/App.css';
 import Formulario from './components/Formulario';
 import Tabela from './components/Tabela';
-
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produto, setProduto] = useState([]);
+
+  useEffect(()=>{
+    fetch("http://localhost:8080/listar")
+    .then(retorno=retorno.json)
+    .then(retorno_convertido=>setProduto(retorno_convertido));
+  })
+
   return (
-    <div className="App">
-    <Formulario/>
+    <div>
+    <p>{JSON.stringify(produto)}</p>
+    <Formulario botao={btnCadastrar}/>
     <Tabela/>
     </div>
   );
